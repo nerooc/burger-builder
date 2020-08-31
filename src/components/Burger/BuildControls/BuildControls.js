d48b483fd108d67e8f9b5124de45f22148d20648
Tomasz Gajda <tomaszgajdait@gmail.com>
2020-08-31 23:20:21 +0200

    started working on BuildControls component

---
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import 'BuildControls.mudule.css';
+const buildControls = (props) => (
+    <div>
+        {/*build controls will be placed here*/}
+    </div>  
+); 
+
+export default buildControls;
