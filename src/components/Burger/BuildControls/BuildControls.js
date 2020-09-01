fbfbc854a1010a41a288d2e505fe2c6017fd5241
Tomasz Gajda <tomaszgajdait@gmail.com>
2020-09-01 11:13:37 +0200

    ingredient adding mechanic

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -1,8 +1,23 @@
 import React from 'react';
-import 'BuildControls.mudule.css';
+
+import classes from './BuildControls.module.css';
+import BuildControl from './BuildControl/BuildControl';
+
+const controls = [
+    {label: 'Salad', type: 'salad'},
+    {label: 'Bacon', type: 'bacon'},
+    {label: 'Cheese', type: 'cheese'},
+    {label: 'Meat', type: 'meat'},
+];
+
 const buildControls = (props) => (
-    <div>
-        {/*build controls will be placed here*/}
+    <div className = {classes.BuildControls}>
+        {controls.map(ctrl => (
+            <BuildControl 
+                key={ctrl.label} 
+                label={ctrl.label}
+                added={() => props.ingredientAdded(ctrl.type)}/>
+        ))}
     </div>  
 ); 
 
