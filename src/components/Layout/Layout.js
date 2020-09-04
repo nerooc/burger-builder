c9a6a7bf262ff44c1c554a2a0d6098184c9812df
Tomasz Gajda <tomasz_gajda@outlook.com>
2020-09-04 20:26:21 +0200

    side drawer toggle

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -14,10 +14,17 @@ class Layout extends React.Component{
         this.setState({showSideDrawer:false});
     }
 
+    sideDrawerToggleHandler = () => {
+        this.setState((prevState) => {
+            return { showSideDrawer: !prevState.showSideDrawer }
+        }
+        );
+    }
+
     render(){
         return(
             <Aux>
-                <Toolbar />
+                <Toolbar toggle = {this.sideDrawerToggleHandler} />
                 <SideDrawer open = {this.state.showSideDrawer} closed = {this.sideDrawerClosedHandler} />
                 <main className = {classes.Content}>
                     {this.props.children}
--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import classes from './Toolbar.module.css';
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
+import DrawerToggle from '../SideDrawer/DrawerToggle/DrawerToggle';
+
+import classes from './Toolbar.module.css';
+
 
 const toolbar = (props) => (
     <header className={classes.Toolbar}>
-        <div>MENU</div>
+        <DrawerToggle clicked={props.toggle}/>
         <div className={classes.Logo}>
             <Logo />
         </div>
