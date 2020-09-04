170a83783e9698ba6c2cdf8d9292844e6dec6a56
Tomasz Gajda <tomasz_gajda@outlook.com>
2020-09-04 20:06:53 +0200

    added hiding mechanic for sidedrawer

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -1,18 +1,26 @@
 import React from 'react';
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
+import Backdrop from '../../UI/Backdrop/Backdrop';
+import Auxilliary from '../../../hoc/Auxilliary';
+
 import classes from './SideDrawer.module.css';
 
 const sideDrawer = (props) => {
+    const attachedClasses = [classes.SideDrawer, props.open ? classes.Open : classes.Close].join(' ');
+    
     return (
-        <div className={classes.SideDrawer}>
-            <div className={classes.Logo}>
-                <Logo />
+        <Auxilliary>
+            <Backdrop show={props.open} clicked={props.closed}/>
+            <div className={attachedClasses}>
+                <div className={classes.Logo}>
+                    <Logo />
+                </div>
+                <nav>
+                    <NavigationItems />
+                </nav>
             </div>
-            <nav>
-                <NavigationItems />
-            </nav>
-        </div>
+        </Auxilliary>
     );
 };
 
